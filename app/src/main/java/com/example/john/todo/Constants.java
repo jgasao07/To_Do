035500b2a3e22ab3e817271eee801fc1b4270c7e
John Gasao <gasao@wisc.edu>
2016-04-27 23:09:54 -0500

    Initial commit

---
+++ app/src/main/java/com/example/john/todo/Constants.java
@@ -0,0 +1,8 @@
+package com.example.john.todo;
+
+/**
+ * Created by JOHN on 4/27/2016.
+ */
+public class Constants {
+    public static final String FIREBASE_URL = "https://to-do-app-tutorial.firebaseio.com";
+}
