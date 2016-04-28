2d2ba8ffbae58fcd3b36090cd30eace4a0aae1b5
jgasao07 <gasao@wisc.edu>
2016-04-28 16:33:00 -0500

    Added user creation and data validation

--- app/src/main/java/com/example/john/todo/MainActivity.java
+++ app/src/main/java/com/example/john/todo/MainActivity.java
@@ -2,19 +2,26 @@ package com.example.john.todo;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
 public class MainActivity extends AppCompatActivity {
 
     private Firebase mRef;
+    private String mUserId;
+    private String itemsUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +35,59 @@ public class MainActivity extends AppCompatActivity {
         if (mRef.getAuth() == null) {
             loadLoginView();
         }
+
+        try {
+            mUserId = mRef.getAuth().getUid();
+        }
+        catch (Exception e) {
+            loadLoginView();
+        }
+
+        itemsUrl = Constants.FIREBASE_URL + "/users/" + mUserId + "/items";
+
+        // Set Up ListView
+        final ListView listView = (ListView) findViewById(R.id.listView);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
+                android.R.id.text1);
+        listView.setAdapter(adapter);
+
+        // Add items via the Button and EditText at the bottom of the view
+        final EditText text = (EditText)findViewById(R.id.todoText);
+        final Button button = (Button) findViewById(R.id.addButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Firebase(itemsUrl).push().child("title").setValue(text.getText().toString());
+            }
+        });
+
+        // Use Firebase to poulate the list
+        new Firebase(itemsUrl).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                adapter.add((String) dataSnapshot.child("title").getValue());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                adapter.remove((String) dataSnapshot.child("title").getValue());
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
     }
 
     @Override
