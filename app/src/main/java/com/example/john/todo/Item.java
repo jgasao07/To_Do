dab9bcd00f9443a71baf6b6d0590ee06472b706e
John Gasao <gasao@wisc.edu>
2016-04-29 12:53:28 -0500

    Item object class

---
+++ app/src/main/java/com/example/john/todo/Item.java
@@ -0,0 +1,23 @@
+package com.example.john.todo;
+
+/**
+ * Created by JOHN on 4/28/2016.
+ */
+public class Item {
+
+    private String title;
+
+    public Item() {}
+
+    public Item(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
